3371e32c42840739d8fea25ecf74cba7f5103c26
Carrie Pascale <cpascale43@gmail.com>
2020-04-29 16:08:28 -0400

    solved: fizzbuzz & chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// create empty array to hold chunks called 'chunked'
+// for each element in the unchunked array
+// retrieve the last element in 'chunked'
+// if last element does not exist or its length === chunk size
+// push a new chunk (subarray) into 'chunked' with the current element
+// else add the current element into the chunk
+
+// function chunk(array, size) {
+//   let chunked = []
+
+//   array.forEach(el => {
+//     let last = chunked[chunked.length-1]
+//     if (!last || last.length === size) {
+//       chunked.push([el])
+//     } else {
+//       last.push(el)
+//     }
+//   })
+
+//   return chunked;
+// }
+
+// create empty chunked array
+// create 'index' start at 0
+// while 'index' is less than array.length
+// push a slice of length 'size' from 'array' into chunked
+// add 'size' to 'index'
+
+function chunk(array, size) {
+  let chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    const chunk = array.slice(index, index+size)
+    chunked.push(chunk)
+    index += size;
+  }
+
+  return chunked
+}
 
 module.exports = chunk;
