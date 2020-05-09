bce9a742ee1b99e6bf3fb38e0201f71c4c7fc2cc
Carrie Pascale <cpascale43@gmail.com>
2020-05-09 16:57:45 -0400

    solved: vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,28 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   str = str.toLowerCase()
+//   let count = 0;
+//   let vowelCheck = ['a', 'e', 'i', 'o', 'u']
+
+//   for (let char of str) {
+//     if (vowelCheck.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// str.match() returns an array containing the matches, or null
+// /[aeiou]/gi -> search for vowels, globally and case-insensitive
+// return the length of str.match()
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
