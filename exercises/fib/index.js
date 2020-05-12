9837c0f2514fc93728e82ae8d980d27a79afb2c6
Carrie Pascale <cpascale43@gmail.com>
2020-05-12 19:55:06 -0400

    solved: fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,31 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// create an array
+// assemble all of the numbers up to that number
+// return the element at that position in the array
+
+// function fib(n) {
+//   let result = [0, 1]
+
+//   for (let i = 2; i <= n; i++) { // O(n) linear time
+//     const a = result[i-1]
+//     const b = result[i-2]
+
+//     result.push(a + b)
+//   }
+
+//   return result[n]
+// }
+
+function fib(n) {
+  if (n === 0) {
+    return 0
+  }
+  if (n === 1 || n === 2) {
+    return 1
+  }
+  return fib(n-2) + fib(n-1)
+}
 
 module.exports = fib;
