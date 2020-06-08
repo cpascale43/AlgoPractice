4fb48574fb0447f8df0ba43e76cbac18948ef99c
Carrie Pascale <cpascale43@gmail.com>
2020-06-08 12:20:44 -0400

    solved: qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,45 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.one = new Stack();
+    this.two = new Stack();
+  }
+
+  add(x) {
+    this.one.push(x);
+  }
+
+  remove() {
+    while (this.one.peek()) {
+      this.two.push(this.one.pop());
+    }
+
+    const x = this.two.pop();
+
+    while (this.two.peek()) {
+      this.one.push(this.two.pop());
+    }
+
+    return x;
+  }
+
+  peek() {
+    while (this.one.peek()) {
+      this.two.push(this.one.pop());
+    }
+
+    const x = this.two.peek();
+
+    while (this.two.peek()) {
+      this.one.push(this.two.pop());
+    }
+
+    return x;
+  }
+}
 
 module.exports = Queue;
