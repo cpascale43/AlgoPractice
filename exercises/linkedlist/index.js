ed80b6c0d41bb28b713e8aa3e1e9f63394acef47
Carrie Pascale <cpascale43@gmail.com>
2020-06-10 18:32:25 -0400

    in process: linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,38 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(x) {
+    let node = new Node(x, this.head)
+
+    this.head = node;
+  }
+
+  size() {
+    let count = 0;
+    let start = this.head;
+    while (start) {
+      count++;
+      start = start.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    let first = this.head;
+    return first;
+  }
+}
 
 module.exports = { Node, LinkedList };
