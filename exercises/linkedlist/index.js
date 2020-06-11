09175eed34141ecfa4cddaaa4dd2fba34ddec174
Carrie Pascale <cpascale43@gmail.com>
2020-06-11 14:01:35 -0400

    in progress: linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -34,6 +34,48 @@ class LinkedList {
     let first = this.head;
     return first;
   }
+
+  getLast() {
+    if (!this.head) return;
+    let start = this.head;
+
+    while (start) {
+      if (!start.next) {
+        return start;
+      }
+      start = start.next
+    }
+  }
+
+  clear() {
+      this.head = null;
+  }
+
+  removeFirst() {
+    let second = this.head.next;
+
+    this.head = second;
+  }
+
+  removeLast() {
+    if (!this.head) return;
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+
 }
 
 module.exports = { Node, LinkedList };
