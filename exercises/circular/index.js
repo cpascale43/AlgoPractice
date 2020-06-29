d315b15b49515a17ff70c4c98b5f67b940cd572e
Carrie Pascale <cpascale43@gmail.com>
2020-06-29 16:26:36 -0400

    solved: circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,20 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    let first = list.getFirst()
+    let second = list.getFirst()
+
+    while (second.next && second.next.next) {
+        first = first.next;
+        second = second.next.next;
+
+        if (first === second) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 module.exports = circular;
