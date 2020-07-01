7199e55ae6252271c6b3a243c4638caf3d9dd9a2
Carrie Pascale <cpascale43@gmail.com>
2020-07-01 11:59:45 -0400

    solved: fromlast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,29 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  // initialize both pointers at first element
+  // then move fast forward n spaces
+  // move slow and fast forward
+  // ask is fast pointing at the last element of the list?
+  // if no, move both forward one more time
+  // if yes, as soon as fast is pointing at the last element,
+  // that means slow must be n elements behind it and pointing at our answer
+
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+      slow = slow.next;
+      fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
